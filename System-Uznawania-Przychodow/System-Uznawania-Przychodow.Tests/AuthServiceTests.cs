f41a5d9b16a3d2e4f69a53081e1874a6aa73ce4d
Oleh Korsun <oleg.korsun.0505@gmail.com>
2025-06-20 01:54:38 +0200

    Dodalem projekt testow. DOdalem Swaggera

---
+++ System-Uznawania-Przychodow/System-Uznawania-Przychodow.Tests/AuthServiceTests.cs
@@ -0,0 +1,6 @@
+namespace System_Uznawania_Przychodow.Tests;
+
+public class AuthServiceTests
+{
+    
+}
--- System-Uznawania-Przychodow/System-Uznawania-Przychodow/Program.cs
+++ System-Uznawania-Przychodow/System-Uznawania-Przychodow/Program.cs
@@ -59,7 +59,40 @@ builder.Services.AddAuthorization();
 
 
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "System Uznawania Przychodów", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Podaj token w formacie: Bearer {token}"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 
 
@@ -75,6 +108,8 @@ app.UseMiddleware<ErrorHandlingMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthentication();
